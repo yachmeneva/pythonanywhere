cd93ffb3b499b0eaec6c4d73de890d46f426372f
Olga Yachmeneva <yaolga@MacBook-Air-Olga-2.local>
2023-02-23 19:10:06 +0300

    should_be_login_page method

---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from .locators import LoginPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login/" in self.browser.current_url, "This is not login page. "
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "No login form presented on login page. "
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "No registrattion form presented on login page. "
